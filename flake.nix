4306264f47b1689a3fb779695b815f7146451fbe
Swhitzer <Swhitzer@yandex.ru>
2025-02-26 16:07:56 +0400

    Some ricing and prepare for restructure

--- flake.nix
+++ flake.nix
@@ -1,31 +1,48 @@
 {
-	description = "My system configuration";
+  description = "My system configuration";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-		home-manager = {
-			url = "github:nix-community/home-manager/release-24.11";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-		#nixvim = {
-		#	url = "github:nix-community/nixvim/nixos-24.11";
-		#	inputs.nixpkgs.follows = "nixpkgs";
-		#};		
-	};
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };		
+  };
 
-	outputs = { nixpkgs, home-manager, ...}:
-		let 
-			system = "x86_64-linux";
-		in {
-			nixosConfigurations.swhit = nixpkgs.lib.nixosSystem {
-			inherit system;
-			modules = [ ./configuration.nix ];
-		};
+  outputs = { self, nixpkgs, home-manager, nixvim, ...}:
+    let 
+    system = "x86_64-linux";
 
-		homeConfigurations.swhit = home-manager.lib.homeManagerConfiguration {
-			pkgs = nixpkgs.legacyPackages.${system};
-			modules = [ ./home.nix ];
-		};
-	};
+  pkgs = import nixpkgs {
+    inherit system;
+
+    config = {
+      allowUnfree = true;
+    };
+  };
+  in {
+    nixosConfigurations.swhit = nixpkgs.lib.nixosSystem {
+      inherit pkgs;
+      inherit system;
+      modules = [ ./system/configuration.nix ];
+    };
+
+    homeConfigurations.swhit = home-manager.lib.homeManagerConfiguration {
+#pkgs = nixpkgs.legacyPackages.${system};
+      inherit pkgs;
+      extraSpecialArgs = {
+	inherit nixvim;
+      };
+      modules = [
+	./home/home.nix
+      ];
+    };
+  };
 }
+
+
